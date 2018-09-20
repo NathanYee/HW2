72e3a8f49f7f55370698232ee433dec4303dfcbf
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-20 18:00:43 -0400

    added adder testbench

--- adder.t.v
+++ adder.t.v
@@ -10,5 +10,22 @@ module testFullAdder();
 
     initial begin
         // Your test code here
+        $display("a b cin | cout sum | Ecout Esum");
+        a=0;b=0;carryin=0; #1000
+        $display("%b %b %b   |    %b   %b | 0     0", a,b,carryin,carryout,sum);
+        a=0;b=0;carryin=1; #1000
+        $display("%b %b %b   |    %b   %b | 0     1", a,b,carryin,carryout,sum);
+        a=0;b=1;carryin=0; #1000
+        $display("%b %b %b   |    %b   %b | 0     1", a,b,carryin,carryout,sum);
+        a=0;b=1;carryin=1; #1000
+        $display("%b %b %b   |    %b   %b | 1     0", a,b,carryin,carryout,sum);
+        a=1;b=0;carryin=0; #1000
+        $display("%b %b %b   |    %b   %b | 0     1", a,b,carryin,carryout,sum);
+        a=1;b=0;carryin=1; #1000
+        $display("%b %b %b   |    %b   %b | 1     0", a,b,carryin,carryout,sum);
+        a=1;b=1;carryin=0; #1000
+        $display("%b %b %b   |    %b   %b | 1     0", a,b,carryin,carryout,sum);
+        a=1;b=1;carryin=1; #1000
+        $display("%b %b %b   |    %b   %b | 1     1", a,b,carryin,carryout,sum);
     end
 endmodule
