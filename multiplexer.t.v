a0bd169e46681e60da89b81428b6a4056da32e75
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-20 00:00:39 -0400

    test bench for multiplexer

--- multiplexer.t.v
+++ multiplexer.t.v
@@ -4,4 +4,33 @@
 
 module testMultiplexer ();
   // Your test code here
+  reg addr0, addr1;
+  reg in0, in1, in2, in3;
+  wire out;
+
+  behavioralMultiplexer multiplexer (out,addr0,addr1,in0,in1,in2,in3);
+  //structuralDecoder multiplexer (out,addr0,addr1,in0,in1,in2,in3); // Swap after testing
+
+  initial begin
+  $display("addr0 addr1 in0 in1 in2 in3 | out | Expected Output");
+  addr0=0;addr1=0;in0=0;in1=0;in2=0;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 0", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=0;addr1=0;in0=1;in1=0;in2=0;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 1", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=1;addr1=0;in0=0;in1=0;in2=0;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 0", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=1;addr1=0;in0=0;in1=1;in2=0;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 1", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=0;addr1=1;in0=0;in1=0;in2=0;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 0", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=0;addr1=1;in0=0;in1=0;in2=1;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 1", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=1;addr1=1;in0=0;in1=0;in2=0;in3=0; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 0", addr0,addr1,in0,in1,in2,in3,out);
+  addr0=1;addr1=1;in0=0;in1=0;in2=0;in3=1; #1000 
+  $display("    %b     %b   %b   %b   %b   %b |   %b | 1", addr0,addr1,in0,in1,in2,in3,out);
+  end
+
+
+
 endmodule
