064acc2fa11e693497c73903b3bee13bf08219a7
nathanyee <nathaniel.yee@students.olin.edu>
2018-09-20 20:08:53 -0400

    did decoder

--- decoder.v
+++ decoder.v
@@ -1,4 +1,7 @@
 // Decoder circuit
+`define AND and #50
+`define OR or #50
+`define NOT not #50
 
 module behavioralDecoder
 (
@@ -17,6 +20,18 @@ module structuralDecoder
     input address0, address1,
     input enable
 );
-    // Your decoder code here
+    // Wire Inversions
+    wire naddress0;
+    wire naddress1;
+    `NOT a0Inv(naddress0, address0);
+    `NOT a1Inv(naddress1, address1);
+
+    // Wire the appropriate address gates and the enable using a 3 input "AND" gate
+    `AND out0gate(out0, naddress0, naddress1, enable);
+    `AND out1gate(out1, address0, naddress1, enable);
+    `AND out2gate(out2, naddress0, address1, enable);
+    `AND out3gate(out3, address0, address1, enable);
+
+
 endmodule
 
